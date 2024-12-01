d65baca6eb148fdfb2788b87afd707f17f8bb170
Yan-Xiangjun <xmascreater@163.com>
2024-12-01 17:48:01 +0800

    更新install.py

--- install.py
+++ install.py
@@ -13,23 +13,27 @@ if not os.path.exists('ltrans_env'):
     os.system(f'{python_path} -m venv ltrans_env')
 
 print('安装依赖库……')
-pip_path = '.\\ltrans_env\\Scripts\\pip3' if os_type == 'Windows' else './ltrans_env/bin/pip3'
-os.system(f'{python_path} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip')
-os.system(f'{pip_path} install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
-os.system(f'{pip_path} install -i https://mirrors.ustc.edu.cn/pypi/simple openai pyside2 pynput pyperclip pyyaml')
+scripts_path = '.\\ltrans_env\\Scripts\\' if os_type == 'Windows' else './ltrans_env/bin/'
+os.system(f"{scripts_path}{python_path[python_path.rfind('python'):]} -m pip install -U -i https://mirrors.ustc.edu.cn/pypi/simple pip")
+os.system(f'{scripts_path}pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple wheel')
+os.system(f'{scripts_path}pip3 install -i https://mirrors.ustc.edu.cn/pypi/simple openai pyside2 pynput pyperclip pyyaml')
 
 print('创建启动脚本……')
 this_dir = os.path.dirname(os.path.realpath(__file__))
 if os_type == 'Windows':
     with open('Ltranslator启动器.cmd', 'w') as launcher_file:
-        launcher_file.write(f'{this_dir}\\ltrans_env\\Scripts\\python3 {this_dir}\\main.py')
+        launcher_file.write(f'{this_dir}\\{scripts_path}python.exe {this_dir}\\main.py')
 elif os_type == 'Darwin':
     with open(f'Ltranslator启动器.command', 'w') as launcher_file:
-        launcher_file.write(f'#!/bin/bash\nset -x\nsudo {this_dir}/ltrans_env/bin/python3 {this_dir}/main.py')
+        launcher_file.write('#!/bin/bash\n'
+                            'set -x\n'
+                            f'sudo {this_dir}/{scripts_path}python3 {this_dir}/main.py')
     os.system(f'chmod +x Ltranslator启动器.command')
 else:  # Linux
     with open('Ltranslator启动器.sh', 'w') as launcher_file:
-        launcher_file.write(f'#!/bin/bash\nset -x\n{this_dir}/ltrans_env/bin/python3 {this_dir}/main.py')
+        launcher_file.write('#!/bin/bash\n'
+                            'set -x\n'
+                            f'{this_dir}/{scripts_path}python3 {this_dir}/main.py')
     os.system('chmod +x Ltranslator启动器.sh')
 
 print('完成！')
