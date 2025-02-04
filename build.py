c89f97329c4a2eda1c03c12d1a536bdc543654b1
Yan-Xiangjun <xmascreater@163.com>
2025-02-04 23:25:45 +0800

    添加shell=True

--- build.py
+++ build.py
@@ -19,17 +19,19 @@ if len(sys.argv) >= 3:
 print('Creating a virtual environment ...')
 python_name = sys.executable[sys.executable.rfind('python'):]
 if not os.path.exists(env_path):
-    subprocess.run(f'{python_name} -m venv {env_path}', check=True)
+    subprocess.run(f'{python_name} -m venv {env_path}', check=True, shell=True)
 
 print('Installing dependencies ...')
 scripts_path = f'{env_path}\\Scripts\\' if os_type == 'Windows' else f'{env_path}/bin/'
 url = 'https://mirrors.ustc.edu.cn/pypi/simple'
-subprocess.run(f"{scripts_path}{python_name} -m pip install -U -i {url} pip", check=True)
-subprocess.run(f'{scripts_path}pip3 install -i {url} wheel', check=True)
-subprocess.run(f'{scripts_path}pip3 install -i {url} -r requirements.txt', check=True)
+subprocess.run(f"{scripts_path}{python_name} -m pip install -U -i {url} pip",
+               check=True,
+               shell=True)
+subprocess.run(f'{scripts_path}pip3 install -i {url} wheel', check=True, shell=True)
+subprocess.run(f'{scripts_path}pip3 install -i {url} -r requirements.txt', check=True, shell=True)
 
 print('Packing ...')
-subprocess.run(f'{scripts_path}pyinstaller -y --noconsole main.py', check=True)
+subprocess.run(f'{scripts_path}pyinstaller -y --noconsole main.py', check=True, shell=True)
 
 print('Copying configuration file ...')
 shutil.copy('config.yml', os.path.join('dist', 'main', 'config.yml'))
